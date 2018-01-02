94c0867b5f28e7e0f92f87e83f5ff4f21ce2d944
SevenZHOU <zhoubighead@gmail.com>
2018-01-02 17:53:46 +0800

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<string>
+
+class Animal{
+public:
+    virtual void eat()=0;
+    void setWeight(int n){weight=n;}
+    void setLength(int n){length=n;}
+
+    Animal(int w,int l,std::string c):weight(w),length(l),color(c){
+        std::cout<<"Animal created! "<<std::endl;
+    }
+    Animal(){
+        std::cout<<"Anmal default created"<<std::endl;
+    }
+
+    int getWeight(){return weight;}
+    int getLength(){return length;}
+    virtual ~Animal(){
+        std::cout<<"Animal destroyed."<<std::endl;
+    }
+
+
+private:
+    int weight;
+    int length;
+
+protected:
+    std::string color;
+};
+
+class FourLegs{
+public:
+    FourLegs(){
+        std::cout<<"FourLegs created."<<std::endl;
+    }
+    void run(){
+        std::cout<<"FourLegs can run!"<<std::endl;
+    }
+    virtual void jump()=0;
+    virtual ~FourLegs(){
+        std::cout<<"Fourlegs destoryed"<<std::endl;
+    }
+protected:
+    int height;
+};
+
+class Dog:public Animal,public FourLegs{
+public:
+    Dog(int w,int l,int s,std::string c):Animal(w,l,c),runSpeed(s){
+        std::cout<<"Dog created! "<<std::endl;
+    }
+    Dog(){
+        std::cout<<"Dog default created!"<<std::endl;
+    }
+
+    void eat(){
+        std::cout<<"Dog eat bones!"<<std::endl;
+    }
+
+    void jump(){
+        std::cout<<"Dog can jump! "<<std::endl;
+    }
+    void bark(){
+        std::cout<<"Dog bark! "<<std::endl;
+    }
+    void setHeight(int h){
+        height=h;
+    }
+
+    void broatcast(){
+        std::cout<<"Weight: "<<getWeight()<<" kg"<<std::endl;
+        std::cout<<"Lenght: "<<getLength()<<" cm"<<std::endl;
+        std::cout<<"Color: "<<color<<std::endl;
+        std::cout<<"Height: "<<height<<" cm"<<std::endl;
+        std::cout<<"runspeed: "<<runSpeed<<std::endl;
+    }
+    ~Dog(){
+        std::cout<<"Dog destoryed"<<std::endl;
+    }
+
+private:
+    int runSpeed;
+};
+
+int main(){
+    Dog Wangcai(26,60,80,"Brown");
+    Animal *p= new Dog;
+    Wangcai.run();
+    Wangcai.bark();
+    Wangcai.jump();
+    Wangcai.setHeight(60);
+    Wangcai.broatcast();
+    delete p;
+    return 0;
+}
